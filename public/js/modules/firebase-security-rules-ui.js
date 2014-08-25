850a393837ae9b55c1746b4fe79c74933cb56e13
Daniel Aranda <javierdaniel@gmail.com>
2014-08-25 00:03:27 -0400

    Sample code for define and use a module

--- public/js/modules/firebase-security-rules-ui.js
+++ public/js/modules/firebase-security-rules-ui.js
@@ -2,11 +2,36 @@ require.config({
 
     baseUrl: 'javascript',
 
+    shim : {
+        backbone : {
+            deps : ['jquery', 'underscore'],
+            exports : 'Backbone'
+        },
+        underscore : {
+            exports : '_'
+        }
+    },
+
     paths: {
-        jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js',
-        underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
-        backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js'
+        jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
+        underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+        backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min'
     }
 
 });
-console.log('hola');
+
+/**
+ *
+ *
+define(
+    'fsr-ui/test',
+    [],
+    function(){
+        return {a : 'b'};
+    }
+);
+
+require(['backbone', 'fsr-ui/test'], function(Backbone, app){
+    console.log('yeah',app);
+});
+*/
