e948891cca50699f6d9293881e9eabd6e96ebea9
Daniel Aranda <javierdaniel@gmail.com>
2014-08-26 22:44:36 -0400

    js concat file created

---
+++ public/js/modules/your-project-name_0.9.0.js
@@ -0,0 +1,39 @@
+$d = {
+    project : 'your-project-name'
+};
+require.config({
+
+    baseUrl: 'js/',
+
+    shim : {
+        backbone : {
+            deps : ['jquery', 'underscore'],
+            exports : 'Backbone'
+        },
+        underscore : {
+            exports : '_'
+        },
+        'plugins' : {
+            deps : ['jquery', 'backbone']
+        }
+    },
+
+    paths: {
+        jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
+        underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+        backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+        domReady: '//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady',
+        plugins: 'plugins'
+    }
+
+});
+define(
+    $d.project + '/App',
+    [],
+    function(){
+
+        var app;
+
+        return app;
+    }
+);
