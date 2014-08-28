5785a17a08f33f1629513631356550a74ae93b70
Daniel Aranda <javierdaniel@gmail.com>
2014-08-28 09:13:01 -0400

    using beforeEach to load class

--- source/tests/test.js
+++ source/tests/test.js
@@ -1,14 +1,21 @@
 describe('hello', function(){
 
-    it('world ', function(done){
+    var app;
 
+    beforeEach(function(done){
 
-        require([$d.project + '/App'], function (app) {
+        require([$d.project + '/App'], function (_app) {
 
-            expect(app.name).toBe('hello');
+            app = _app;
             done();
 
         });
+
+    });
+
+    it('world ', function(){
+
+        expect(app.name).toBe('hello');
 
     });
 
